5b8409155cad023426344e493117d2fc7225bfb0
Lucas Alencar <v55411la@H-LOSX0GDX6.local>
2023-12-05 18:00:33 +0000

    Primeira tentativa de criar os thiessen polygons usando o R.
    Sem sucesso

--- scripts/data.R
+++ scripts/data.R
@@ -5,28 +5,38 @@
 library(readxl)
 library(here)
 library(dplyr)
-library(tidyr)
+library(deldir)
+library(sf)
+library(sp)
 
 #data----
-read.csv(file = "data/IML_90-10/Base_marginacion_localidad_90-10.csv", fileEncoding = "latin1") -> IML_90_10
-read_xls("data/IML_2020/IML_2020.xls", sheet = "IML_2020_AGS-MEX") %>% 
-  rbind(., read_xls("data/IML_2020/IML_2020.xls", sheet = "IML_2020_MICH-ZAC")) %>% 
-  rbind(., read_xls("data/IML_2020//IML_2020.xls", sheet = "IML_2020_LOC2viv")) -> IML_2020
+read_xlsx(path = here("data/IRSL_localidades_00_20_clean.xlsx"), sheet = 1) -> IRSL_1
+read_xlsx(path = here("data/IRSL_localidades_00_20_clean.xlsx"), sheet = 2) -> IRSL_2
+read.csv(file = "/Users/user/Library/CloudStorage/OneDrive-TheUniversityofManchester/SFT/Data/Mexico/clean/statistical/localidad_locations_clean.csv") -> locality_points
 
 ##organization----
-IML_90_10 %>% 
-  pivot_wider(id_cols = c(CVE_ENT, NOM_ENT, CVE_MUN, NOM_MUN, CVE_LOC, NOM_LOC),
-              names_from = AÑO,
-              values_from = POB_TOT:GML) %>%
-  glimpse()-> IML_90_10_wide
+IRSL_1 %>% 
+  rbind(IRSL_2) %>% 
+  mutate(locality_code = as.integer(locality_code)) %>% 
+  left_join(y = select(locality_points, locality_code, AMBITO, LAT_DECIMAL, LON_DECIMAL, ALTITUD, TOTAL.DE.VIVIENDAS.HABITADAS), 
+            by = "locality_code") %>% 
+  rename("viviendas_2020" = "TOTAL.DE.VIVIENDAS.HABITADAS" ) %>% 
+  mutate(locality_code = as.factor(locality_code),
+         across(.cols = c(GRSL_2000:AMBITO), .fns = as.factor),
+         viviendas_2020 = as.double(viviendas_2020)) %>% 
+  glimpse -> IRSL
 
-IML_2020 %>% 
-  mutate(CVE_LOC = as.numeric(CVE_LOC)) %>% 
-  glimpse
+###Thiessen polygons----
+IRSL %>% 
+  filter(LAT_DECIMAL != 'NA') %>% 
+  st_as_sf(coords = c('LON_DECIMAL', 'LAT_DECIMAL')) %>%
+  st_set_crs('6364') %>%
+  st_union %>% 
+  glimpse -> points_IRSL
 
-IML_90_10 %>% 
-  select(CVE_LOC, AÑO) %>% 
-  group_by(AÑO) %>% 
-  summarise(n = n()) %>% 
-  unique()
-  glimpse()
+st_voronoi(points_IRSL) -> thiessen_IRSL
+
+plot(thiessen_IRSL)
+
+deldir(x = na.omit(IRSL$LON_DECIMAL), y = na.omit(IRSL$LAT_DECIMAL)) -> thiessen_IRSL
+plot(thiessen_IRSL)

f3be1b843f582ba1f7a92ca4fd4519fb1850a129
alenc09 <alencar.lucasc@gmail.com>
2024-02-06 16:15:31 +0000

    Started the script to build the full table

--- scripts/data_full.R
+++ scripts/data_full.R
@@ -2,3 +2,12 @@
 #Script to build full table for mexico-IRSL analysis
 
 #Library----
+library(sf)
+library(dplyr)
+
+#data----
+read_sf("/Users/user/Library/CloudStorage/OneDrive-TheUniversityofManchester/SFT/Data/Mexico/clean/GIS/thiessen_IRSL_all_ITRF92-LCC.shp") -> thiessen
+
+##organization----
+thiessen %>% 
+  glimpse

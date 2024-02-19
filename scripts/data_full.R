d6e990c2bbc752e8a7cdac922b401442c5ad3224
alenc09 <alencar.lucasc@gmail.com>
2024-02-19 17:30:48 +0000

    Star working on the land cover data

--- scripts/data_full.R
+++ scripts/data_full.R
@@ -7,11 +7,11 @@ library(dplyr)
 library(tidyr)
 
 #data----
-read_sf("/Users/user/Library/CloudStorage/OneDrive-TheUniversityofManchester/SFT/Data/Mexico/clean/GIS/thiessen_IRSL_all_ITRF92-LCC.shp") -> thiessen
-read_sf("/Users/user/Library/CloudStorage/OneDrive-TheUniversityofManchester/SFT/Data/Mexico/clean/GIS/thiessen_LC_2000.shp") -> thiessen_LC_2000
-read_sf("/Users/user/Library/CloudStorage/OneDrive-TheUniversityofManchester/SFT/Data/Mexico/clean/GIS/thiessen_LC_2005.shp") -> thiessen_LC_2005
-read_sf("/Users/user/Library/CloudStorage/OneDrive-TheUniversityofManchester/SFT/Data/Mexico/clean/GIS/thiessen_LC_2010.shp") -> thiessen_LC_2010
-read_sf("/Users/user/Library/CloudStorage/OneDrive-TheUniversityofManchester/SFT/Data/Mexico/clean/GIS/thiessen_LC_2020.shp") -> thiessen_LC_2020
+read_sf("/Users/user/Library/CloudStorage/OneDrive-TheUniversityofManchester/SFT/Data/Mexico/clean/GIS/thissen_all.shp") -> thiessen_all
+read_sf("/Users/user/Library/CloudStorage/OneDrive-TheUniversityofManchester/SFT/Data/Mexico/clean/GIS/thiessen_all_LC_2000.shp") -> thiessen_LC_2000
+read_sf("/Users/user/Library/CloudStorage/OneDrive-TheUniversityofManchester/SFT/Data/Mexico/clean/GIS/thiessen_all_LC_2005.shp") -> thiessen_LC_2005
+read_sf("/Users/user/Library/CloudStorage/OneDrive-TheUniversityofManchester/SFT/Data/Mexico/clean/GIS/thiessen_all_LC_2010.shp") -> thiessen_LC_2010
+read_sf("/Users/user/Library/CloudStorage/OneDrive-TheUniversityofManchester/SFT/Data/Mexico/clean/GIS/thiessen_all_LC_2020.shp") -> thiessen_LC_2020
 
 ##organization----
 ###land cover----
@@ -34,5 +34,12 @@ apply_code <- function(dataframe) {
 
 modified_list <- lapply(list_LC, apply_code)
 
+modified_list[[1]] %>% 
+  data.frame() %>% glimpse #-> thiessen_LC_2000_wider
+modified_list[[2]] -> thiessen_LC_2005_wider
+modified_list[[3]] -> thiessen_LC_2010_wider
+modified_list[[4]] -> thiessen_LC_2020_wider
 
-
+thiessen_LC_2000_wider %>% 
+  left_join(thiessen_LC_2005_wider, by = "locality_c") %>% 
+  glimpse

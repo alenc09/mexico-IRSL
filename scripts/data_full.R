75fd71e6dd017f718c1edc74d7cecfea7485081d
alenc09 <alencar.lucasc@gmail.com>
2024-02-19 15:27:33 +0000

    Reorganized localities data to build thiessen polygons based on localities from INEGI 2020 national census

--- scripts/data_full.R
+++ scripts/data_full.R
@@ -4,10 +4,35 @@
 #Library----
 library(sf)
 library(dplyr)
+library(tidyr)
 
 #data----
 read_sf("/Users/user/Library/CloudStorage/OneDrive-TheUniversityofManchester/SFT/Data/Mexico/clean/GIS/thiessen_IRSL_all_ITRF92-LCC.shp") -> thiessen
+read_sf("/Users/user/Library/CloudStorage/OneDrive-TheUniversityofManchester/SFT/Data/Mexico/clean/GIS/thiessen_LC_2000.shp") -> thiessen_LC_2000
+read_sf("/Users/user/Library/CloudStorage/OneDrive-TheUniversityofManchester/SFT/Data/Mexico/clean/GIS/thiessen_LC_2005.shp") -> thiessen_LC_2005
+read_sf("/Users/user/Library/CloudStorage/OneDrive-TheUniversityofManchester/SFT/Data/Mexico/clean/GIS/thiessen_LC_2010.shp") -> thiessen_LC_2010
+read_sf("/Users/user/Library/CloudStorage/OneDrive-TheUniversityofManchester/SFT/Data/Mexico/clean/GIS/thiessen_LC_2020.shp") -> thiessen_LC_2020
 
 ##organization----
-thiessen %>% 
-  glimpse
+###land cover----
+list(thiessen_LC_2000, thiessen_LC_2005, thiessen_LC_2010, 
+     rename(.data = thiessen_LC_2020, descripcio = DESCRIPCIO)) -> list_LC
+
+apply_code <- function(dataframe) {
+  # Apply pivot_wider and replace NA with 0
+  thiessen_LC_ <- dataframe %>%
+    as.data.frame() %>% 
+    pivot_wider(id_cols = "locality_c",
+                names_from = c("descripcio", "year"),
+                values_from = "area_m",
+                values_fn = sum) %>%
+    mutate(across(.col = 2:length(.),
+                  ~ replace_na(data = ., 0)))
+  # Return the modified dataframe and year for naming
+  return(list(dataframe = thiessen_LC_))
+}
+
+modified_list <- lapply(list_LC, apply_code)
+
+
+
--- scripts/data_lat-long.R
+++ scripts/data_lat-long.R
@@ -12,16 +12,15 @@ library(landscapemetrics)
 library(writexl)
 
 #data----
-read_xlsx(path = here("data/IRSL_localidades_00_20_clean.xlsx"), sheet = 1) -> IRSL_1
-read_xlsx(path = here("data/IRSL_localidades_00_20_clean.xlsx"), sheet = 2) -> IRSL_2
+read_xlsx(path = "/Users/user/Library/CloudStorage/OneDrive-TheUniversityofManchester/SFT/Data/Mexico/clean/statistical/IRSL_localidades_00_20_clean.xlsx", sheet = 1) -> IRSL_1
+read_xlsx(path = "/Users/user/Library/CloudStorage/OneDrive-TheUniversityofManchester/SFT/Data/Mexico/clean/statistical/IRSL_localidades_00_20_clean.xlsx", sheet = 2) -> IRSL_2
 read.csv(file = "/Users/user/Library/CloudStorage/OneDrive-TheUniversityofManchester/SFT/Data/Mexico/clean/statistical/localidad_locations_clean.csv") -> locality_points
-st_read(dsn = "/Users/user/Library/CloudStorage/OneDrive-TheUniversityofManchester/SFT/Data/Mexico/clean/GIS/thiessen_clipped_IRSL.shp") ->thiessen_clipped
-raster(x = "/Users/user/Library/CloudStorage/OneDrive-TheUniversityofManchester/SFT/Data/Mexico/clean/GIS/land_cover_2010_nad83.tif") -> lc_2010_nad83
+read.csv(file = "/Users/user/Library/CloudStorage/OneDrive-TheUniversityofManchester/SFT/Data/Mexico/raw/Statistical/censo_2020/ITER_NALCSV20.csv") -> localities_2020
 
 ##organization----
 IRSL_1 %>% 
   rbind(IRSL_2) %>% 
-  mutate(locality_code = as.integer(locality_code)) %>% 
+  mutate(locality_code = as.integer(locality_code)) %>%
   left_join(y = dplyr::select(locality_points, locality_code, AMBITO, LAT_DECIMAL, LON_DECIMAL, ALTITUD, TOTAL.DE.VIVIENDAS.HABITADAS),
             by = "locality_code") %>%
   rename("viviendas_2020" = "TOTAL.DE.VIVIENDAS.HABITADAS" ) %>%
@@ -33,30 +32,7 @@ IRSL_1 %>%
 rowSums(!is.na(IRSL_full[,c("IRSL_2000","IRSL_2005","IRSL_2010","IRSL_2020")])) -> count
 IRSL_full[count>=2,] -> IRSL
 
+
 ###Localities' points----
-IRSL_1 %>% 
-  rbind(IRSL_2) %>% 
-  mutate(locality_code = as.integer(locality_code)) %>% 
-  left_join(y = dplyr::select(locality_points, locality_code, AMBITO, LAT_DECIMAL, LON_DECIMAL, ALTITUD, TOTAL.DE.VIVIENDAS.HABITADAS),
-            by = "locality_code") %>%
-  rename("viviendas_2020" = "TOTAL.DE.VIVIENDAS.HABITADAS" ) %>%
-  glimpse %>% 
-  write.csv(x = ., file="/Users/user/Library/CloudStorage/OneDrive-TheUniversityofManchester/SFT/Data/Mexico/clean/GIS/locality_IRSL_points.csv")
-
-###land_cover data----
-check_landscape(lc_2010_nad83) #couldn't check. Session aborted in every try
-sample_lsm(landscape = lc_2010_nad83,
-            y = thiessen_clipped,
-            plot_id = thiessen_clipped$lclty_c,
-            what = c("lsm_c_ca", "lsm_c_pland", "lsm_l_shdi")) -> thiessen_lc
-
-write.csv(x = thiessen_lc, file = "/Users/user/Library/CloudStorage/OneDrive-TheUniversityofManchester/SFT/Data/Mexico/analysis/thiessen_lc.xlsx")
-
-###ejidos----
-points_IRSL %>% 
-  dplyr::select(-IRSL_2005) %>% 
-  filter(IRSL_2000 != 'NA',
-         IRSL_2010 != 'NA',
-         IRSL_2020 != 'NA',
-         AMBITO == 'R') %>% 
-  glimpse()
+localities_2020 %>% 
+  glimpse
